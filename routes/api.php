ad9805f3da36e6ecb2f4673632731966857391ed
ramazan <Rama5duf@yandex.ru>
2024-11-15 00:37:45 +0300

    Добавил фильтрацию, сортировку и Категорию для товаров

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,8 @@ Route::prefix('v1')->group(function () {
 		Route::get( 'me',       'me')->middleware('auth:sanctum');
 
 	});
+
+	Route::apiResource('categories', CategoryController::class);
 });
 
 
